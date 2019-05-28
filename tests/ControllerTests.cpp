17eb503a95cecb31f31324e72b3d52046fd33c4e
Łukasz Ziobroń <lukasz@coders.school>
2019-05-28 09:48:49 +0200

    Initial BDD tests structure

--- tests/ControllerTests.cpp
+++ tests/ControllerTests.cpp
@@ -1,3 +1,19 @@
 #include "external/catch.hpp"
-#include "external/hippomocks.h"
 #include "Controller.hpp"
+
+
+SCENARIO("Controller - updateRpm() behavior", "[Controller]") {
+    GIVEN("A Controller, StubThermometer, Fan, target temperature = 0.0, tolerance = 1.0") {
+
+        WHEN("Temperature is 0.0") {
+            THEN("Fan speed is 1000") {
+            }
+        }
+
+        WHEN("Temperature is -1.01") {
+            THEN("Fan speed is 0") {
+            }
+        }
+
+    }
+}
